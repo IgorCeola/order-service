05ad31796a7807e254d68cad8501734020344251
IgorCeola <igorceola@hotmail.com>
2025-11-28 15:35:24 -0300

    Adicionando inicialização do Prometheus

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from controllers.order_controller import router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Order Service")
+
+@app.on_event("startup")
+def startup_event():
+    Instrumentator().instrument(
+        app, 
+        exclude_paths=["/metrics"], 
+    ).expose(app)
 
 app.include_router(router)
 
